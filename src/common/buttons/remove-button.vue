2911734ce2978327dd8501ed1784bda6d2e195d6
Connor Walsh <cwalsh@organikweb.com.au>
2022-10-03 17:10:51 +0800

    Add common form controls as own files

---
+++ src/common/buttons/remove-button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button :class="buttonClass" class="text-mediumGray">
+    <img src="../../assets/icon-cross.svg" />
+  </button>
+</template>
+<script>
+export default {
+  name: "RemoveButton",
+  props: {
+    buttonClass: String,
+  },
+};
+</script>
---
+++ src/common/form/text-input.vue
@@ -0,0 +1,30 @@
+<template>
+  <label
+    v-if="this.label"
+    for="title"
+    class="text-xs text-mediumGray font-bold mb-2"
+    >{{ label }}</label
+  >
+  <input
+    type="text"
+    name="title"
+    :value="value"
+    :class="inputClass"
+    class="w-full border border-linesLight border-opacity-80 py-2 px-4 text-base font-medium text-black rounded"
+  />
+</template>
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    label: String,
+    labelClass: String,
+    inputClass: String,
+    value: String,
+    placeholder: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
