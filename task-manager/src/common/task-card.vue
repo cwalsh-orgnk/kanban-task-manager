26562e7711d353f15cbd19e6877c3968238e5e64
Connor Walsh <cwalsh@organikweb.com.au>
2022-10-18 15:49:26 +0800

    Rename showTaskDetails modal

--- task-manager/src/common/task-card.vue
+++ task-manager/src/common/task-card.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     class="task py-6 px-4 flex flex-col justify-center items-start text-left bg-white mb-5 rounded-lg w-full max-w-[calc(100%-1rem)] shadow"
-    @click="showModal"
+    @click="showTaskDetailsModal"
   >
     <h4 class="text-md font-bold text-black max-w-[250px]">{{ task.title }}</h4>
     <h5 class="text-xs text-mediumGray font-bold mt-1">
@@ -75,7 +75,7 @@ export default {
     openTask(task) {
       return task;
     },
-    showModal() {
+    showTaskDetailsModal() {
       this.isDetailsModalVisible = true;
     },
     haslistUpdated(haslistUpdated) {
